8bb4cfe721c2bd3ac4f84a3f2f4d6165efbddba3
Fabricio Novicki <fabricionovicki@hotmail.com>
2025-08-24 23:40:14 -0300

    Header

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
     
 
     for (let i = 0; i < buttons.length; i++) {
@@ -40,4 +53,14 @@ function escondeTodasAbas() {
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
+}
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
